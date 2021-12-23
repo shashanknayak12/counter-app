ca244f80a1dd55e0bae5daa958ea3b1d54f103fa
Shashank nayak <shashanknayak.1209@gmail.com>
2021-12-23 17:44:58 +0530

    added nav-bar which displays counter number

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counters from './components/counters'
-import { Component } from 'react/cjs/react.development';
 
 ReactDOM.render(
-  < Counters />,
+  < App />,
   document.getElementById('root')
 );
 
